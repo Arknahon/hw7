f4f40bd97b3b936d58e9e0baec9714e25e1607a1
Arknahon <bakirov090303@gmail.com>
2023-01-27 23:55:33 +0600

    Initial Commit

---
+++ 4M1stHomeWork/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  4M1stHomeWork
+//
+//  Created by user on 27/1/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
